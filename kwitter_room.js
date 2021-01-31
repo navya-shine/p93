cc8f30cf388cdf28014afbc2a0303da6cb54e7ce
navya-shine <69377970+navya-shine@users.noreply.github.com>
2021-01-31 21:45:12 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDqPZmJm_sUhqKqJm7XLtjurRSSN63hs20",
+    authDomain: "leys-cha.firebaseapp.com",
+    projectId: "leys-cha",
+    storageBucket: "leys-cha.appspot.com",
+    messagingSenderId: "334936352091",
+    appId: "1:334936352091:web:60e7023ee7881279caa67e"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
